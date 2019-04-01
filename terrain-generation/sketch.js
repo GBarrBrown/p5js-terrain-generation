f8b88a3bdfacb62d4e79b420f7587ee674b9b825
gbarrbrown <gbb7899@gmail.com>
2019-04-01 23:41:28 +1300

    random z index and 2d array of random nums

--- terrain-generation/sketch.js
+++ terrain-generation/sketch.js
@@ -1,4 +1,4 @@
-let cols, rows, w, h;
+let cols, rows, w, h, terrain;
 let scale = 20
 
 function setup() {
@@ -8,6 +8,8 @@ function setup() {
   h = 600
   cols = w / scale
   rows = h / scale
+  terrain = [...Array(cols)].map(() => Array(rows).fill(random(-10,10))) // 2d Array
+  console.log(terrain)
 }
 
 function draw() {
@@ -19,11 +21,12 @@ function draw() {
 
   rotateX(PI/3)
   translate(-w/2, -h/2)
+  frameRate(1)
 
   for (let y = 0; y < rows; y++) {
     beginShape(TRIANGLE_STRIP)
     for (let x = 0; x < cols; x++) {
-      vertex(x*scale, y*scale)
+      vertex(x*scale, y*scale, random(-10, 10))
       vertex(x*scale, (y+1)*scale)
       // rect(x*scale, y*scale, scale, scale)
     }
